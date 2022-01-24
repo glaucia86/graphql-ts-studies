b49aa8b75e0a22a5533597aab8336ce027c970d1
glaucia86 <glaucia_lemos86@hotmail.com>
2022-01-23 23:26:42 -0300

    creating the schema example.

--- project-01/src/index.js
+++ project-01/src/index.js
@@ -4,3 +4,30 @@
  * date: 01/23/2022
  * author: Glaucia Lemos <@glaucia_lemos86>
  */
+
+const { ApolloServer, gql } = require('apollo-server');
+
+const typeDefs = gql`
+type Query {
+  sessions: [Session]
+}
+  type Session {
+    id: ID!,
+    title: String!,
+    description: String,
+    startAt: String,
+    endsAt: String,
+    room: String,
+    day: String,
+    format: String,
+    track: String,
+    level: String
+  }`
+
+const server = new ApolloServer({ typeDefs });
+
+server
+  .listen()
+  .then(({ url }) => {
+    console.log(`GraphQL is running on ${url}`);
+  });
