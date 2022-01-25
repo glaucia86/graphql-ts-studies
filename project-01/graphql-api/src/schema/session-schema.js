5f6d9c10bc29e2dd84348f95a47b644634f6e003
glaucia86 <glaucia_lemos86@hotmail.com>
2022-01-25 20:19:31 -0300

    creating a Session Input

--- project-01/graphql-api/src/schema/session-schema.js
+++ project-01/graphql-api/src/schema/session-schema.js
@@ -27,12 +27,25 @@ module.exports = gql`
   }
   type Mutation {
     toggleFavoriteSession(id: ID): Session
+    addNewSession(session: SessionInput): Session
   }
   type Speaker {
     id: ID!
     bio: String
     name: String
     sessions: [Session]
+  }
+  input SessionInput {
+    title: String!
+    description: String
+    startsAt: String
+    endsAt: String
+    room: String
+    day: String
+    format: String
+    track: String
+    level: String
+    favorite: Boolean
   }
   type Session {
     id: ID!
