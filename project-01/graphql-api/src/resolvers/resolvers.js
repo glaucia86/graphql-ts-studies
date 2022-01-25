12a095513dd9c7efa42f6e7d0eb66984552cdfb6
glaucia86 <glaucia_lemos86@hotmail.com>
2022-01-25 11:36:27 -0300

    including some methods to the speaker query

--- project-01/graphql-api/src/resolvers/resolvers.js
+++ project-01/graphql-api/src/resolvers/resolvers.js
@@ -7,11 +7,17 @@
 
 module.exports = {
   Query: {
-    sessions: (parent, args, { dataSources }, info) => {
+    sessions: (parent, args, { dataSources }) => {
       return dataSources.sessionAPI.getSessions(args);
     },
-    sessionById: (parent, { id }, { dataSources }, info) => {
+    sessionById: (parent, { id }, { dataSources }) => {
       return dataSources.sessionAPI.getSessionById(id);
     },
+    speakers: (parent, args, { dataSources }) => {
+      return dataSources.speakerAPI.getSpeakers(args);
+    },
+    speakersById: (parent, { id }, { dataSources }) => {
+      return dataSources.speakerAPI.getSpeakerById(id);
+    }
   },
 };
