d55fbd925f010ebf69617023b8676c536ab61931
glaucia86 <glaucia_lemos86@hotmail.com>
2022-02-02 11:44:09 -0300

    creating a new Mutation: createComment

--- project-02/graphql-basics/src/index.js
+++ project-02/graphql-basics/src/index.js
@@ -35,13 +35,13 @@ const posts = [{
   id: '2',
   title: 'Learn TypeScript',
   body: 'Become a Ninja in TypeScript in few months',
-  published: false,
+  published: true,
   author: '1'
 }, {
   id: '3',
   title: 'Learn Node.Js',
   body: 'Become a Ninja in Node in few months',
-  published: true,
+  published: false,
   author: '2'
 }];
 
@@ -79,6 +79,8 @@ const typeDefs = `
 
   type Mutation {
     createUser(name: String!, email: String!, age: Int): User!
+    createPost(title: String!, body: String!, published: Boolean!, author: ID!): Post!
+    createComment(text: String!, author: ID!, post: ID!): Comment!
   }
 
   type User {
@@ -168,6 +170,42 @@ const resolvers = {
 
       users.push(user);
       return user;
+    },
+    createPost(parent, args, ctx, info) {
+      const userExists = users.some((user) => user.id === args.author);
+
+      if (!userExists) {
+        throw new Error('User not found.');
+      }
+
+      const post = {
+        id: uuidv4(),
+        title: args.title,
+        body: args.body,
+        published: args.published,
+        author: args.author,
+      }
+
+      posts.push(post);
+      return post;
+    },
+    createComment(parent, args, ctx, info) {
+      const userExists = users.some((user) => user.id === args.author);
+      const postExists = posts.some((post) => post.id === args.post && post.published);
+
+      if(!userExists || !postExists) {
+        throw new Error('Unable to find User and Post.')
+      }
+
+      const comment = {
+        id: uuidv4(),
+        text: args.text,
+        author: args.author,
+        post: args.post
+      }
+
+      comments.push(comment);
+      return comment;
     }
   },
   Post: {
