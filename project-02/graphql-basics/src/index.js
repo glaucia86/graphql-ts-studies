07b705dfbe013e3ad77ee0bf4f5b2235ee1866fe
glaucia86 <glaucia_lemos86@hotmail.com>
2022-02-02 11:52:01 -0300

    adding babel transform object spread operator package

--- project-02/graphql-basics/src/index.js
+++ project-02/graphql-basics/src/index.js
@@ -163,9 +163,7 @@ const resolvers = {
 
       const user = {
         id: uuidv4(),
-        name: args.name,
-        email: args.email,
-        age: args.age
+        ...args
       }
 
       users.push(user);
@@ -180,10 +178,7 @@ const resolvers = {
 
       const post = {
         id: uuidv4(),
-        title: args.title,
-        body: args.body,
-        published: args.published,
-        author: args.author,
+        ...args
       }
 
       posts.push(post);
@@ -199,9 +194,7 @@ const resolvers = {
 
       const comment = {
         id: uuidv4(),
-        text: args.text,
-        author: args.author,
-        post: args.post
+        ...args
       }
 
       comments.push(comment);
