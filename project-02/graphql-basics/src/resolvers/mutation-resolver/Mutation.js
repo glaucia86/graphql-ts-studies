0ae328551cad68def6e5c895c94c165e1e3f3d36
glaucia86 <glaucia_lemos86@hotmail.com>
2022-02-05 00:50:52 -0300

    creating more 2 mutations: updatePost and updateComment

--- project-02/graphql-basics/src/resolvers/mutation-resolver/Mutation.js
+++ project-02/graphql-basics/src/resolvers/mutation-resolver/Mutation.js
@@ -94,6 +94,29 @@ const Mutation = {
     return post;
   },
 
+  updatePost(parent, args, { db }, info) {
+    const { id, data } = args;
+    const post = db.posts.find((post) => post.id === id);
+
+    if(!post) {
+      throw new Error('Post not found');
+    }
+
+    if (typeof data.title === 'string') {
+      post.title = data.title
+    }
+
+    if (typeof data.body === 'string') {
+      post.body = data.body
+    }
+
+    if (typeof data.published === 'boolean') {
+      post.published = data.published
+    }
+
+    return post;
+  },
+
   deletePost(parent, args, { db }, info) {
     const postIndex = db.posts.findIndex((post) => post.id === args.id);
 
@@ -136,7 +159,22 @@ const Mutation = {
     const deletedComments = db.comments.splice(commentIndex, 1);
 
     return deletedComments[0];
-  }
+  },
+
+  updateComment(parent, args, { db }, info) {
+    const { id, data } = args;
+    const comment = db.comments.find((comment) => comment.id === id);
+
+    if(!comment) {
+      throw new Error('Comment not found!');
+    }
+
+    if (typeof data.text === 'string') {
+      comment.text = data.text;
+    }
+
+    return comment;
+  },
 };
 
 export { Mutation as default };
