ea69646bdf203632cf0b1431401eb6f5e402d1c6
glaucia86 <glaucia_lemos86@hotmail.com>
2022-02-04 23:40:15 -0300

    removing unecessary files and folders.

--- project-02/graphql-basics/study-import-export/index.js
+++
@@ -1,9 +0,0 @@
-import myCurrentLocation, { getGreeting, message, name } from './mymodule';
-import AddNumbers, { subtract } from './math';
-
-console.log(message);
-console.log(name);
-console.log(myCurrentLocation);
-console.log(getGreeting('Prince'));
-console.log(AddNumbers(2,8));
-console.log(subtract(10,5));
--- project-02/graphql-basics/study-import-export/math.js
+++
@@ -1,9 +0,0 @@
-const add = (number_1, number_2) => {
-  return number_1 + number_2;
-};
-
-const subtract = (number_1, number_2) => {
-  return number_1 - number_2;
-};
-
-export { subtract, add as default }
