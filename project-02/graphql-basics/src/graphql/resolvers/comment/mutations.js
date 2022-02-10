948f856a1a184170b7e37882523be66de4608dce
glaucia86 <glaucia_lemos86@hotmail.com>
2022-02-10 14:46:30 -0300

    creating the logic to created, delete and updating a comment subscription.

--- project-02/graphql-basics/src/graphql/resolvers/comment/mutations.js
+++ project-02/graphql-basics/src/graphql/resolvers/comment/mutations.js
@@ -11,47 +11,70 @@ import { v4 as uuidv4 } from 'uuid';
 const commentMutations = {
   createComment: (parent, args, { db, pubsub }, info) => {
     const userExists = db.users.some((user) => user.id === args.data.author);
-    const postExists = db.posts.some((post) => post.id === args.data.post && post.published);
+    const postExists = db.posts.some(
+      (post) => post.id === args.data.post && post.published
+    );
 
-    if(!userExists || !postExists) {
-      throw new Error('Unable to find User and Post.')
+    if (!userExists || !postExists) {
+      throw new Error('Unable to find User and Post.');
     }
 
     const comment = {
       id: uuidv4(),
-      ...args.data
-    }
+      ...args.data,
+    };
 
     db.comments.push(comment);
 
-    pubsub.publish(`comment ${args.data.post}`, { comment });
+    pubsub.publish(`comment ${args.data.post}`, {
+      comment: {
+        mutation: 'CREATED',
+        data: comment,
+      },
+    });
 
     return comment;
   },
 
-  deleteComment: (parent, args, { db }, info) => {
-    const commentIndex = db.comments.findIndex((comment) => comment.id === args.id);
+  deleteComment: (parent, args, { db, pubsub }, info) => {
+    const commentIndex = db.comments.findIndex(
+      (comment) => comment.id === args.id
+    );
 
     if (commentIndex === -1) {
       throw new Error('Comment not found');
     }
 
-    const deletedComments = db.comments.splice(commentIndex, 1);
+    const [deletedComment] = db.comments.splice(commentIndex, 1);
 
-    return deletedComments[0];
+    pubsub.publish(`comment ${deletedComment.post}`, {
+      comment: {
+        mutation: 'DELETED',
+        data: deletedComment
+      }
+    });
+
+    return deletedComment;
   },
 
-  updateComment: (parent, args, { db }, info) => {
+  updateComment: (parent, args, { db, pubsub }, info) => {
     const { id, data } = args;
     const comment = db.comments.find((comment) => comment.id === id);
 
-    if(!comment) {
+    if (!comment) {
       throw new Error('Comment not found!');
     }
 
     if (typeof data.text === 'string') {
       comment.text = data.text;
     }
+
+    pubsub.publish(`comment ${comment.post}`, {
+      comment: {
+        mutation: 'UPDATED',
+        data: comment
+      }
+    });
 
     return comment;
   },
