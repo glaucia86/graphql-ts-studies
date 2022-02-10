0e6d02535f70d9badf1993bdaa9eb316dfc033b4
glaucia86 <glaucia_lemos86@hotmail.com>
2022-02-10 13:17:44 -0300

    creating the logic to created, delete and updating a post subscription.

--- project-02/graphql-basics/src/graphql/resolvers/post/mutations.js
+++ project-02/graphql-basics/src/graphql/resolvers/post/mutations.js
@@ -17,53 +17,91 @@ const postMutations = {
 
     const post = {
       id: uuidv4(),
-      ...args.data
-    }
+      ...args.data,
+    };
 
     db.posts.push(post);
 
     if (args.data.author) {
-      pubsub.publish('post', { post });
+      pubsub.publish('post', {
+        post: {
+          mutation: 'CREATED',
+          data: post,
+        },
+      });
     }
 
     return post;
   },
 
-  updatePost: (parent, args, { db }, info) => {
+  updatePost: (parent, args, { db, pubsub }, info) => {
     const { id, data } = args;
     const post = db.posts.find((post) => post.id === id);
+    const originalPost = { ...post };
 
-    if(!post) {
+    if (!post) {
       throw new Error('Post not found');
     }
 
     if (typeof data.title === 'string') {
-      post.title = data.title
+      post.title = data.title;
     }
 
     if (typeof data.body === 'string') {
-      post.body = data.body
+      post.body = data.body;
     }
 
     if (typeof data.published === 'boolean') {
-      post.published = data.published
+      post.published = data.published;
+
+      if (originalPost.published && !post.published) {
+        pubsub.publish('post', {
+          post: {
+            mutation: 'DELETED',
+            data: originalPost,
+          },
+        });
+      } else if (!originalPost.published && post.published) {
+        pubsub.publish('post', {
+          post: {
+            mutation: 'CREATED',
+            data: post,
+          },
+        });
+      }
+    } else if (post.published) {
+      pubsub.publish('post', {
+        post: {
+          mutation: 'UPDATED',
+          data: post,
+        },
+      });
     }
 
     return post;
   },
 
-  deletePost: (parent, args, { db }, info) => {
+  deletePost: (parent, args, { db, pubsub }, info) => {
     const postIndex = db.posts.findIndex((post) => post.id === args.id);
 
     if (postIndex === -1) {
       throw new Error('Post not found');
     }
 
-    const deletedPosts = db.posts.splice(postIndex, 1);
+    const [post] = db.posts.splice(postIndex, 1);
 
-    db.comments = db.comments.filter((comment) => comment.post !== args.id)
+    db.comments = db.comments.filter((comment) => comment.post !== args.id);
 
-    return deletedPosts[0];
+    if (post.published) {
+      pubsub.publish('post', {
+        post: {
+          mutation: 'DELETED',
+          data: post,
+        },
+      });
+    }
+
+    return post;
   },
 };
 
