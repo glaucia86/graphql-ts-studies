ad429931d0e13968257b30b3807be214a0244868
glaucia86 <glaucia_lemos86@hotmail.com>
2022-02-10 12:32:04 -0300

    removing the property 'count' - was just for test.

--- project-02/graphql-basics/src/graphql/resolvers/comment/subscriptions.js
+++ project-02/graphql-basics/src/graphql/resolvers/comment/subscriptions.js
@@ -7,21 +7,6 @@
  */
 
 const commentSubscriptions = {
-    count: {
-      subscribe: (parent, args, { pubsub }, info) => {
-        let count = 0;
-
-        setInterval(() => {
-          count++;
-          pubsub.publish('count', {
-            count
-          })
-        },  1000);
-
-        return pubsub.asyncIterator('count');
-      }
-    },
-
     comment: {
       subscribe: (parent, { postId }, { db, pubsub }, info) => {
         const post = db.posts.find((post) => post.id === postId && post.published);
--- project-02/graphql-basics/src/graphql/resolvers/post/subscriptions.js
+++ project-02/graphql-basics/src/graphql/resolvers/post/subscriptions.js
@@ -7,21 +7,6 @@
  */
 
 const postSubscriptions = {
-  count: {
-    subscribe: (parent, args, { pubsub }, info) => {
-      let count = 0;
-
-      setInterval(() => {
-        count++;
-        pubsub.publish('count', {
-          count,
-        });
-      }, 1000);
-
-      return pubsub.asyncIterator('count');
-    },
-  },
-
   post: {
     subscribe: (parent, args, { pubsub }, info) => {
       return pubsub.asyncIterator('post');
