3c457fea0fc8cc94d9f1bfb62e0547d59d0fed99
glaucia86 <glaucia_lemos86@hotmail.com>
2022-02-17 12:40:19 -0300

    creating the Category Logic

--- project-03/ecommerce-app/src/index.js
+++ project-03/ecommerce-app/src/index.js
@@ -91,20 +91,43 @@ const products = [
   },
 ];
 
+const categories = [
+  {
+    id: "c01b1ff4-f894-4ef2-b27a-22aacc2fca70",
+    name: "Kitchen",
+  },
+  {
+    id: "34115aac-0ff5-4859-8f43-10e8db23602b",
+    name: "Garden",
+  },
+  {
+    id: "d914aec0-25b2-4103-9ed8-225d39018d1d",
+    name: "Sports",
+  },
+];
+
 const typeDefs = gql`
   type Query {
     hello: String!
     products: [Product!]!
     product(id: ID!): Product
+    categories: [Category!]!
+    category(id: ID!): Category
   }
 
   type Product {
+    id: ID!
     name: String!
     description: String!
     image: String!
     quantity: Int!
     price: Float!
     onSale: Boolean!
+  }
+
+  type Category {
+    id: ID!
+    name: String!
   }
 `;
 
@@ -119,12 +142,14 @@ const resolvers = {
     },
 
     product: (parent, args, ctx) => {
-      const productId = args.id;
-      const product = products.find(product => product.id === productId);
+      const { id } = args;
+      return products.find(product => product.id === id);
+    },
 
-      if (!product) return null;
-
-      return product;
+    categories: (parent, args, ctx) => categories,
+    category: (parent, args, ctx) => {
+      const { id } = args;
+      return categories.find((category => category.id === id));
     }
   }
 }
