3e650ce4dee3ac98c6119556aa82d36114a8ee66
glaucia86 <glaucia_lemos86@hotmail.com>
2022-06-21 18:57:37 -0300

    cases studies and best practices how to design a GraphQL API

--- project-04/graphql-design/index.js
+++ project-04/graphql-design/index.js
@@ -1,80 +1,11 @@
-const { ApolloServer, gql } = require('apollo-server');
-
-const typeDefs = gql`
-  type Query {
-    cars: [Car!]!
-  }
-
-  type Car {
-    id: ID!
-    color: String!
-    make: String!
-  }
-
-  type Mutation {
-    groupDelete(groupId: ID!)
-    groupPublish(groupId: ID!)
-    groupUnpublish(groupId: ID!)
-    groupAddCars(groupId: ID!, carId: ID!)
-    groupRemoveCars(groupId: ID!, carId: ID!)
-    groupCreate(
-      groupInput: GroupInput!
-    )
-    groupUpdate(
-      groupId: ID!
-      groupInput: GroupInput!
-    )
-  }
-
-  input GroupInput {
-    name: String
-    image: ImageInput
-    description: String
-    featureSet: GroupFeatureFields
-  }
-
-  input ImageInput {
-    url: String!
-  }
-
-  type Group {
-    id: ID!
-    featureSet: GroupFeatureSet
-    hasCar(id: ID!): Boolean!
-    cars(skip: Int!, take: Int!): [Car!]!
-    name: String!
-    image: Image!
-    description: String!
-  }
-
-  type Image {
-    id: ID!
-    url: String!
-  }
-
-  type GroupFeatureSet {
-    features: [GroupFeatures!]!
-    applyFeaturesSeperately: Boolean!
-  }
-
-  type GroupFeatures {
-    feature: GroupFeatureFields!
-  }
-
-  enum GroupFeatureFields {
-    INCLINE_ENGINE
-    FOUR_CYLINDER_ENGINE
-    TWIN_CYLINDER_ENGINE
-    RED_PAINT
-    BLACK_PAINT
-  }
-`;
+const { ApolloServer } = require('apollo-server');
+const { typeDefs } = require('./typeDefs/00-Naive-Schema');
 
 const server = new ApolloServer({
   typeDefs,
   resolvers: {
     Query: {
-      cars: () => [{ id: 1, color: 'blue', make: 'Toyota' }],
+      hello: () => 'World',
     },
   },
 });
