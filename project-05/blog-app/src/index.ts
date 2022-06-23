59ccb4d36a92e5dca43c2d103cacf17bcdc98c1c
glaucia86 <glaucia_lemos86@hotmail.com>
2022-06-23 20:36:10 -0300

    Initial commit - project-05

---
+++ project-05/blog-app/src/index.ts
@@ -0,0 +1,10 @@
+/**
+ * file: src/index.ts
+ * description: file responsible to execute the application.
+ * date: 06/24/2022
+ * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
+ */
+
+const age: number = 35;
+
+console.log(age);
