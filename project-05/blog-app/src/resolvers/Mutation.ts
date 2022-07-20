a524fc0b5ad6c0b8e5b059f97610026e11024900
glaucia86 <glaucia_lemos86@hotmail.com>
2022-07-19 21:48:01 -0300

    creating the postDelete in the Mutation.ts file

--- project-05/blog-app/src/resolvers/Mutation.ts
+++ project-05/blog-app/src/resolvers/Mutation.ts
@@ -51,7 +51,10 @@ export const Mutation = {
     }
   },
 
-  postUpdate: async (_: any, { post, postId }: { postId: string, post: PostArgs["post"] }, { prisma }: Context
+  postUpdate: async (
+    _: any,
+    { post, postId }: { postId: string, post: PostArgs["post"] },
+    { prisma }: Context
   ): Promise<PostPayloadType> => {
     const { title, content } = post;
 
@@ -101,6 +104,39 @@ export const Mutation = {
           id: Number(postId)
         }
       })
+    }
+  },
+
+  postDelete: async (
+    _: any,
+    { postId }: { postId: string },
+    { prisma }: Context): Promise<PostPayloadType> => {
+    const post = await prisma.post.findUnique({
+      where: {
+        id: Number(postId)
+      }
+    })
+
+    if (!post) {
+      return {
+        userErrors: [
+          {
+            message: 'Post does not exists!'
+          }
+        ],
+        post: null
+      }
+    }
+
+    await prisma.post.delete({
+      where: {
+        id: Number(postId)
+      }
+    })
+
+    return {
+      userErrors: [],
+      post
     }
   }
 };
--- project-05/blog-app/src/schema.ts
+++ project-05/blog-app/src/schema.ts
@@ -15,6 +15,7 @@ export const typeDefs = gql`
   type Mutation {
     postCreate(post: PostInput!): PostPayload!
     postUpdate(postId: ID!, post: PostInput!): PostPayload!
+    postDelete(postId: ID!): PostPayload!
   }
 
   type Post {
